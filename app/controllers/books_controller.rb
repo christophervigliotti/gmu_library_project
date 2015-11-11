3860dad3de0b69579b1d1cd6f5a9ce2c29eb08f2
Christopher Vigliotti <christopher@christophervigliotti.com>
2015-11-10 17:58:30 -0800

    up to the end of lab four

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
   def index
+    @available_at = Time.now
+    @books = ["Atlas Sucks","Bender's Game","Green Eggs and Hamlet","Hobbits Take The Long Way"]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  # how do I redirect (so the URL includes '/books')
+  get "/" => "books#index"
   get "/books" => "books#index"
 end
