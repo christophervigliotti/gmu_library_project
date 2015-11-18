ce0e0709a0d5d6b8d2cd045e29998c489c628e40
Christopher Vigliotti <christopher@christophervigliotti.com>
2015-11-18 16:47:33 -0500

    last o the labs

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
   # can I make this redirect?  if so do I do that here?
   get '/' => 'books#index'
 
-  get '/books' => 'books#index', as: 'books'
-  get '/books/new' => 'books#new', as: 'new_book'
-  get '/books/:id' => 'books#show', as: 'book'
-  post '/books' => 'books#create'
-  get '/books/:id/edit' => 'books#edit', as: 'edit_book'
-  patch '/books/:id' => 'books#update'
-  delete '/books/:id' => 'books#destroy'
+  # this one line...
+  resources :books
+  # ...replaces all of this..
+  # get '/books' => 'books#index', as: 'books'
+  # get '/books/new' => 'books#new', as: 'new_book'
+  # get '/books/:id' => 'books#show', as: 'book'
+  # post '/books' => 'books#create'
+  # get '/books/:id/edit' => 'books#edit', as: 'edit_book'
+  # patch '/books/:id' => 'books#update'
+  # delete '/books/:id' => 'books#destroy'
 end
