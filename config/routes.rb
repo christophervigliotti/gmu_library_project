390bd8c4d22e01bc8f3680537e731ba51a82ed57
Christopher Vigliotti <christopher@christophervigliotti.com>
2015-11-24 19:20:25 -0500

    up to lab 6

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,4 @@
 Rails.application.routes.draw do
-  # can I make this redirect?  if so do I do that here?
-  get '/' => 'books#index'
-
-  # this one line...
+  root 'books#index'
   resources :books
-  # ...replaces all of this..
-  # get '/books' => 'books#index', as: 'books'
-  # get '/books/new' => 'books#new', as: 'new_book'
-  # get '/books/:id' => 'books#show', as: 'book'
-  # post '/books' => 'books#create'
-  # get '/books/:id/edit' => 'books#edit', as: 'edit_book'
-  # patch '/books/:id' => 'books#update'
-  # delete '/books/:id' => 'books#destroy'
 end
